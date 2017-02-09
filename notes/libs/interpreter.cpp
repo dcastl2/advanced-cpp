de117c5e963b4c690077a1f2c1fcb5b45a769fc5
Dennis Castleberry <dcastl2@tigers.lsu.edu>
2017-02-09 13:05:39 -0600

    Added interpreter example

---
+++ notes/libs/interpreter.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cmath>
+
+void usage() {
+  std::cout 
+    << "  add a b     - add a and b"                   << std::endl
+    << "  help        - print this message"            << std::endl
+    << "  prime n     - tell if n is prime"            << std::endl
+    << "  random a b  - random number between a and b" << std::endl
+    << "  quit        - quit the interpreter"          
+  ;
+}
+
+
+bool isPrime(int n) {
+  int i;
+  for (i=2; i<=std::sqrt(n); i++)
+      if (n % i == 0)
+         return false;
+  return true;
+}
+
+int main() {
+
+  std::string cmd;
+  int a, b;
+
+  srand(time(NULL) + getpid());
+  std::cout << "> ";
+  while (std::cin >> cmd) {
+    if (cmd == "add") {
+      std::cin >> a;
+      std::cin >> b;
+      std::cout << "< " << a+b;
+    }
+    else if (cmd == "random") {
+      std::cin >> a;
+      std::cin >> b;
+      std::cout << "< " << rand() % (b-a) + a;
+    }
+    else if (cmd == "prime") {
+      std::cin >> a;
+      std::cout << "< " << (isPrime(a) ? "true" : "false");
+    }
+    else if (cmd == "help") {
+      usage();
+    }
+    else if (cmd == "quit") {
+      exit(1);
+    }
+    std::cout << std::endl << "> ";
+  }
+
+}
